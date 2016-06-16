5771746af59a76748be6916a3e7e91353baab5b9
root <root@zb.me>
2016-06-16 10:27:21 +0800

    dbcon

--- dbcon.php
+++ dbcon.php
@@ -1,12 +1,12 @@
 ﻿<?php
 	$hostname = "rds50pn92wn3z3b9z1lk.mysql.rds.aliyuncs.com";
-	$username = "all_read";
-	$password = "weshare12";
+	$username = "report";
+	$password = "report61136500";
 	$dbName = "wershare";
 	
 	define('CLIENT_MULTI_RESULTS',131072);
 	
-	$conn=mysqli_connect($hostname,$username,$password,$dbName) or die("Could not connect: ".mysql_error());
+	$conn=mysqli_connect($hostname,$username,$password,$dbName) or die("Could not connect: ".mysqli_error());
 	
 	// or die("Could not connect: ".$conn->getMessage()."\n");
 	//mysql_select_db($dbName,$conn) or die("Could not select database");
