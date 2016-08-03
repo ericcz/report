947355cb251c39b598033acd0ed0cf4dc7c44810
root <root@zb.me>
2016-08-03 14:26:49 +0800

    20160803.9

--- dataVGet.php
+++ dataVGet.php
@@ -1,22 +1,20 @@
 <?php
 include_once "dbcon.php";
 
-$proc='cspDashboard_detail';
-$dt='2016-06-13';
+$proc='cspDashboard_dataV';
 $dc='';
-$typ='loc';
+$typ=$_REQUEST['typ'];
 $query="select curdate()";
-
 	$result=mysqli_query($conn,"set names utf8");
-	//$result=mysqli_real_query($conn,"call $proc('$typ',@x)");
+	//$result=mysqli_query($conn,$query);
+	$result=mysqli_real_query($conn,"call $proc('$typ')");
 	//$result=mysqli_real_query($conn,"select @x");
-
-	$result=mysqli_query($conn,$query);
 	
 	while($conn->more_results()){
 	$result=mysqli_store_result($conn);
 	$conn->next_result();
 	}
+	
 	if ($result){ 
 		while($row=mysqli_fetch_row($result)){
 			$dc.=$row[0];
@@ -24,8 +22,6 @@ $query="select curdate()";
 	}else
 		$dc = "0";
 
-
-echo '[{"value":234}]';
-
+echo $dc;
 mysqli_close($conn);
 ?>
