b40a897f21b89a29d9ee944322b85ca7851c5b2c
root <root@zb.me>
2016-08-03 10:45:19 +0800

    20160803.1

---
+++ dataVGet.php
@@ -0,0 +1,31 @@
+<?php
+include_once "dbcon.php";
+
+$proc='cspDashboard_detail';
+$dt='2016-06-13';
+$dc='';
+$typ='loc';
+$query="select curdate()";
+
+	$result=mysqli_query($conn,"set names utf8");
+	//$result=mysqli_real_query($conn,"call $proc('$typ',@x)");
+	//$result=mysqli_real_query($conn,"select @x");
+
+	$result=mysqli_query($conn,$query);
+	
+	while($conn->more_results()){
+	$result=mysqli_store_result($conn);
+	$conn->next_result();
+	}
+	if ($result){ 
+		while($row=mysqli_fetch_row($result)){
+			$dc.=$row[0];
+		}
+	}else
+		$dc = "0";
+
+
+echo $dc;
+
+mysqli_close($conn);
+?>
