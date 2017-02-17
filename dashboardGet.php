5c7479d3626b9677a9440b7205e7967474e4527e
root <root@zb.me>
2017-02-17 16:01:12 +0800

    20170217.1

--- dashboardGet.php
+++ dashboardGet.php
@@ -101,15 +101,21 @@ function fnDetails(){
 		$GLOBALS["conn"]->next_result();
 	}
 	switch($typ){
-	case 'debit': $title = '生效日期,有效期,姓名,酒店,售价,赠送金额,结余,用户来源,';
+	case 'debit': $title = '生效日期,有效期,姓名,酒店,售价,赠送金额,结余,是否激活公众号,是否激活app,';
 	break;
-	case 'debit0': $title = '生效日期,有效期,姓名,酒店,售价,赠送金额,结余,用户来源,';
+	case 'debit-m': $title = '酒店,售卡数量,销售金额,赠送金额,已激活公众号,已激活app,';
+	break;
+	case 'debit-all': $title = '酒店,售卡数量,销售金额,赠送金额,结余,已激活公众号,已激活app,';
+	break;
+	case 'debit0': $title = '生效日期,有效期,姓名,酒店,售价,赠送金额,结余,';
+	break;
+	case 'debit0-all': $title = '酒店,退卡数量,退卡金额,退卡赠送金额,退卡结余,';
 	break;
 	case 'member': $title = '加入日期,电话,姓名,酒店,间夜数,剩余换游币,赠送换游币,';
 	break;
 	case 'hotel': $title = '日期,城市,酒店名称,间夜数,可用数,最低价,';
 	break;
-	case 'hotel-sale-d': $title = '日期,酒店,订单内容,订单类型,联系电话,姓名,订单使用日期,订单结束日期,换游币,订单状态,';
+	case 'hotel-sale-d': $title = '日期,酒店,订单内容,订单类型,联系电话,姓名,入住日期,退房日期,换游币,间夜数,';
 	break;
 	default:exit;
 	}
@@ -129,11 +135,6 @@ function fnDetails(){
 	}else
 		$dc = "0";
 	$dc.="</table>";
-	
-	if ($typ=="debit"){
-		$dc.="<input type=checkbox id=chkDebit value=0>&nbsp;显示全部";
-	}
-	
 	return $dc;
 }
 
